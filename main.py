3bc17c94f5ff6ac51b92c07ef5a243273729c320
Pablo Gonzalez <pablo.gonzalezpachon@gmail.com>
2025-12-29 00:24:20 +0100

    Deleted some files

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ from windows_use.agent import Agent, Browser
 from dotenv import load_dotenv
 import os
 
+# Using python 3.13
+
 load_dotenv()
+
 
 def main():
     api_key = os.getenv("MISTRAL_API_KEY")
-    llm=ChatMistral(model='magistral-small-latest',api_key=api_key,temperature=0.7)
+    llm = ChatMistral(model='magistral-small-latest',
+                      api_key=api_key, temperature=0.7)
     # llm=ChatGoogle(model="gemini-2.5-flash-lite",thinking_budget=0, api_key=api_key, temperature=0.7)
     # llm=ChatAnthropic(model="claude-sonnet-4-5", api_key=api_key, temperature=0.7,max_tokens=1000)
     # llm=ChatOllama(model="qwen3-vl:235b-cloud",temperature=0.2)
-    agent = Agent(llm=llm, browser=Browser.EDGE, use_vision=False, auto_minimize=False)
+    agent = Agent(llm=llm, browser=Browser.EDGE,
+                  use_vision=False, auto_minimize=False)
     agent.print_response(query=input("Enter a query: "))
+
 
 if __name__ == "__main__":
     main()
